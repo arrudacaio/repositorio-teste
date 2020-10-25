6f9789a44dd5de3bc4c99aa8269ea27f2bfb083e
Roberta Felix <berttafellix1@gmail.com>
2020-10-25 01:40:07 -0300

    add changess

---
+++ python/divisivel.py
@@ -0,0 +1,13 @@
+cont = 0
+
+divisivel = int(input())
+quant = int(input())
+
+for i in range(quant):
+  num = int(input())
+  if num%divisivel == 0:
+    cont += 1
+  
+porcentagem = (cont/quant) * 100
+
+print(f"{cont} ({porcentagem:.1f}%)")
